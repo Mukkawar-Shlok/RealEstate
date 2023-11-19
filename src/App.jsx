b3f0e63c19568c8d7fdc0064e0b5d478daea97a8
Shlok Mukkawar <shlokmukkawar2001@gmail.com>
2023-11-19 19:05:06 +0530

    header component added

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/sign-in" element={<SignIn/>}></Route>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'fefce8': '#fefce8',
+      },
+    },
   },
   plugins: [],
 }
