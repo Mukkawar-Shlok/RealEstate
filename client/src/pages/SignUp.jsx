8e52abb8d78c577b8e842f37fffb3fb779bebe76
Shlok Mukkawar <shlokmukkawar2001@gmail.com>
2023-11-22 10:32:39 +0530

    added functionality of sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,61 @@
 //for creating the anchor tag
-import {Link} from 'react-router-dom';
+import { useState } from 'react';
+import {Link, useNavigate } from 'react-router-dom';
 function SignUp() {
+  //states
+  const [formData, setFormData] = useState({});
+  const [error,setError] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+  
+  function handelChange(e){
+    setFormData(
+      {
+        //destructure the old data 
+        ...formData,
+        //and set the new data 
+        [e.target.id] : e.target.value
+      }
+    )
+    // console.log(formData);
+  }
+
+ async function handleSubmit(e){
+    e.preventDefault();
+    setLoading(true);
+    const res = await fetch('/api/auth/signup',
+      {
+        method:'POST',
+        headers:{
+          'Content-Type' :'application/json'
+        },
+        body:JSON.stringify(formData)
+      }
+    );
+    const data = await res.json();
+    console.log(data);
+    if(data.sucess == false){
+      setError(data.message);
+      setLoading(false);
+    }else{
+      setError(null);
+      navigate('/sign-in');
+    }
+    setLoading(false);
+
+    console.log(error);
+  }
+  // username,email,password
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-8 ">Sign up</h1>
-      <form action="" className="flex flex-col gap-4">
-        <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username"/>
-        <input type="email" placeholder="Email" className="border p-3 rounded-lg" id="email"/>
-        <input type="password" placeholder="Password" className="border p-3 rounded-lg" id="password"/>
-        <button className="bg-slate-400 rounded-lg uppercase text-white hover:opacity-95 disabled:opacity-80 p-2">Sign Up</button>
+      <form action="" className="flex flex-col gap-4" onSubmit={handleSubmit} >
+        <input type="text" placeholder="username" className="border p-3 rounded-lg" name='username' id="username"  onChange={handelChange}/>
+        <input type="email" placeholder="Email" className="border p-3 rounded-lg" name='email' id="email" onChange={handelChange}/>
+        <input type="password" placeholder="Password" className="border p-3 rounded-lg" name='password' id="password" onChange={handelChange}/>
+        <button className="bg-slate-400 rounded-lg uppercase text-white hover:opacity-95 disabled:opacity-80 p-2" disabled={loading}>
+          {loading ? 'Loading...' : 'Sign up'}
+          </button>
       </form>
       <div className='flex gap-2'>
         <p>Have an account?</p>
@@ -16,6 +63,7 @@ function SignUp() {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-4'>{error}</p>}
     </div>
   )
 }
--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -9,7 +9,7 @@ export default async function signup(req,res,next){
     try{
 
         //if any coming information in incomplete
-        if(!username || !email || !password) return res.status(500).json({message:"Incomplete information"})
+        if(!username || !email || !password) return res.status(500).json({message:"Incomplete information", sucess:false})
 
         // create hashed password 
         const hashedPassword = bcrypt.hashSync(password,10);
@@ -18,14 +18,14 @@ export default async function signup(req,res,next){
         const newUser = new User({username,email,password:hashedPassword});
         await newUser.save()
             .then(()=>{
-                res.status(201).json({message:"User created sucessfully"});
+                res.status(201).json({message:"User created sucessfully",sucess:true});
             }).catch((err)=>{
                 // res.status(500).json({message:"Failed to create user."})
                 next(err);
             })
     }catch(err){
         console.log(err);
-        return res.status(500).json({message:"Internal server error."})
+        return res.status(500).json({message:"Internal server error.", sucess:false})
     }
 
 };
